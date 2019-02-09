5d4021ab181428a7bce9dbfcfb6e529336c216b2
Ryan-Houpt <rehoupt@gmail.com>
2019-02-09 10:38:36 -0600

    login-modal completed

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,5 +1,58 @@
-function onLoadLogin() {
-    alert("Hello!")
-}
+//Variables
 
-$(document).ready(onLoadLogin);
+
+
+//Login Modal Functions
+
+
+//load modal on open
+$(document).ready(function() {
+    $('#login-modal').modal('show');
+});
+
+
+//submit click
+$('#login').click(function() {
+
+    //get user/pass inputs
+
+    var username = $('#username').val();
+    var password = $('#password').val();
+
+    // correct input
+    if (username === "Shaun" && password === "PlusOne") {
+
+        $('#login-modal').modal('hide');
+
+    }
+
+    // incorrect user/pass
+    else if ( username !== "Shaun" && password !== "PlusOne") {
+
+        $('#username-wrong').text("Incorrect Username");
+        $('#password-wrong').text("Incorrect Password");
+
+        $('#username').text("");
+        $('#password').text("");
+
+    }
+
+    // incorrect pass
+    else if ( password !== "PlusOne" ) {
+
+        $('#password-wrong').text("Incorrect Password");
+
+        $('#username').text("");
+        $('#password').text("");
+    }
+
+    //incorrect user
+    else if ( username !== "Shaun" ) {
+
+        $('#username-wrong').text("Incorrect Username");
+
+        $('#username').text("");
+        $('#password').text("");
+
+    }
+});
